e18e806d2550beb25ac202ce0b470540d3d4cc71
ogura943 <kaito.0513.family@gmail.com>
2026-01-19 01:37:33 +0900

    なんか修正してたんでプッシュしとく

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 # app.rb
 require "sinatra"
+require "net/http"
+require "json"
 
 # トップページ
 get "/" do
@@ -13,5 +15,22 @@ end
 
 # 検索画面
 get "/search" do
+  api_key = "YOUR_API_KEY"  # ここにAPIキーを入れてください
+  keyword = params["keyword"] || "渋谷"
+
+  uri = URI("https://webservice.recruit.co.jp/hotpepper/gourmet/v1/")
+  uri.query = URI.encode_www_form({
+    key: api_key,
+    format: "json",
+    keyword: keyword
+  })
+
+  res = Net::HTTP.get_response(uri)
+
+  if res.is_a?(Net::HTTPSuccess)
+    @results = JSON.parse(res.body)
+  else
+    @results = nil
+  end
   erb :search
 end
